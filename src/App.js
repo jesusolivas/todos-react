3d96aeffe777ea7a5a8f4cb748374a686abf39ee
Jesus Olivas <olivasobregon@outlook.com>
2021-05-02 22:47:55 -0700

    adjusted header to always be visible

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import CreateTodo from "./pages/CreateTodo";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Home} exact={true} />
-        <Route path="/create" component={CreateTodo} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/create" component={CreateTodo} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,10 @@
-import Header from "../components/Header";
 import Container from "../components/Container";
 import Todos from "../components/Todos";
 
 const Home = () => (
-    <>
-        <Header />
-        <Container>
-            <Todos />
-        </Container>
-    </>
+    <Container>
+        <Todos />
+    </Container>
 );
 
 export default Home;
