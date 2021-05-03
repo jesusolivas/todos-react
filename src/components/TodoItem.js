4371810164a5d0a30c2d55510fa68618b46f7c9c
Jesus Olivas <olivasobregon@outlook.com>
2021-05-02 22:12:40 -0700

    removed duplicated property

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import "./TodoItem.css";
 
 const TodoItem = ({ description, dueDate, status }) => (
     <div className="todo-item">
-        <div className="todo-item__property">{description + description + description + description}</div>
+        <div className="todo-item__property">{description}</div>
         <div className="todo-item__property">{dueDate}</div>
         <div className="todo-item__property">{status}</div>
     </div>
