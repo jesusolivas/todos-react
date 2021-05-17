cbcdb45cf5f5df0a0056b11729b18d2fab0fff73
Jesus Olivas <olivasobregon@outlook.com>
2021-05-16 22:12:48 -0700

    implemented log out functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,24 @@
+// Third party dependencies
+import { useContext } from "react";
+
+// Internal dependencies
+import AuthContext from "../context/auth";
+import { startLogout } from "../actions/auth";
+
+// Styles
 import "./Header.css"
 
-const Header = () => (
-    <header className="header">TODOS APP</header>
-)
+const Header = () => {
+    const { dispatch } = useContext(AuthContext);
+    return (
+        <header className="header">
+            <div></div>
+            <div>TODOS APP</div>
+            <div>
+                <button className="button" onClick={startLogout.bind(null, dispatch)}>Sign Out</button>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
